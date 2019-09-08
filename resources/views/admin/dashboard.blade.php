2c0a72bfe08e222338a3561d0bfbf460abf81868
Pavel <pavel85_07@bk.ru>
2019-09-08 20:20:54 +0300

    Начало какой-то админки

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="jumbotron">
+                <p>Категории</p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <a href="#" class="btn btn-primary btn-block"> Добавить катеогрию</a>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,11 @@ Route::get('/show', 'TestController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function (){
+    Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+});
