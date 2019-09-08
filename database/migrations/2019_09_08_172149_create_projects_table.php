fc0fb578d3bd9c4e4a8b7e0c7a8e1e087a39800c
Pavel <pavel85_07@bk.ru>
2019-09-08 21:08:08 +0300

    БД проектов, модель, контроллер

---
+++ database/migrations/2019_09_08_172149_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->tinyInteger('published')->nullable();
+            $table->integer('created_by')->nullable();
+            $table->tinyInteger('modified_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
